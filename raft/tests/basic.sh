b5920fc076401586e96ae596be9ec55d0b8f9bbb
nkamil <ninevskykn@gmail.com>
2024-12-18 16:10:42 +0000

    lol

--- raft/tests/basic.sh
+++ raft/tests/basic.sh
@@ -1,5 +1,6 @@
 echo "CREATING: id 1, value 1"
 curl -L 127.0.0.1:8081/1 -d 1
+sleep 1
 echo "\n"
 
 echo "READING 1"
@@ -10,22 +11,27 @@ echo "\n"
 
 echo "CREATING id 2, value 2"
 curl -L 127.0.0.1:8081/2 -d 2
+sleep 1
 echo "\n"
 
 echo "CREATING id 2, value 2"
 curl -L 127.0.0.1:8081/2 -d 2
+sleep 1
 echo "\n"
 
 echo "UPDATING id 2 value 22"
 curl -L -X PUT 127.0.0.1:8081/2 -d 22
+sleep 1
 echo "\n"
 
 echo "CAS id 2 old value 22 value 222"
 curl -L -X PATCH 127.0.0.1:8081/2 -d "22 222"
+sleep 1
 echo "\n"
 
 echo "CAS id 2 old value 22 value 222"
 curl -L -X PATCH 127.0.0.1:8081/2 -d "22 222"
+sleep 1
 echo "\n"
 
 echo "READING 2"
